1398cd0fa473592dacb367a9dbaca983b694139b
chun <1194620498@qq.com>
2020-07-22 10:48:45 +0800

    complete chapter 9

---
+++ ch09/Student_info.h
@@ -0,0 +1,27 @@
+#ifndef GUARD_Student_info
+#define GUARD_Student_info
+
+#include <string>
+#include <vector>
+
+class Student_info
+{
+public:
+	Student_info();
+	Student_info(std::istream &);
+	std::string name() const { return n; }
+	bool valid() const { return !homework.empty(); }
+
+	std::istream &read(std::istream &);
+
+	double grade() const;
+
+private:
+	std::string n;
+	double midterm, final;
+	std::vector<double> homework;
+};
+
+bool compare(const Student_info &, const Student_info &);
+
+#endif
---
+++ ch09/grading_prog.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+using std::max;
+
+#include "Student_info.h"
+#include "median.h"
+
+using std::cin;
+using std::cout;
+using std::domain_error;
+using std::endl;
+using std::setprecision;
+using std::setw;
+using std::sort;
+using std::streamsize;
+using std::string;
+using std::vector;
+
+int main()
+{
+	vector<Student_info> students;
+	Student_info record;
+	string::size_type maxlen = 0;
+
+	while (record.read(cin))
+	{
+		maxlen = max(maxlen, record.name().size());
+		students.push_back(record);
+	}
+	sort(students.begin(), students.end(), compare);
+
+	for (vector<Student_info>::size_type i = 0;
+		 i != students.size(); ++i)
+	{
+		cout << students[i].name()
+			 << string(maxlen + 1 - students[i].name().size(), ' ');
+		try
+		{
+			double final_grade = students[i].grade();
+			streamsize prec = cout.precision();
+			cout << setprecision(3) << final_grade
+				 << setprecision(prec) << endl;
+		}
+		catch (domain_error e)
+		{
+			cout << e.what() << endl;
+		}
+	}
+	return 0;
+}

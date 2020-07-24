0f49e3a892459853fb5ee90fa9f4d4716dd62382
chun <1194620498@qq.com>
2020-07-24 09:43:45 +0800

    complete chapter 12

---
+++ ch12/Student_info.h
@@ -0,0 +1,18 @@
+#ifndef GUARD_Student_info
+#define GUARD_Student_info
+
+#include <iostream>
+#include "Str.h"
+#include "Vec.h"
+
+struct Student_info
+{
+	Str name;
+	double midterm, final;
+	Vec<double> homework;
+};
+
+bool compare(const Student_info &, const Student_info &);
+std::istream &read(std::istream &, Student_info &);
+std::istream &read_hw(std::istream &, Vec<double> &);
+#endif
---
+++ ch12/grade_analysis.cpp
@@ -0,0 +1,41 @@
+#include "Vec.h"
+#include <iostream>
+
+#include "analysis.h"
+#include "Student_info.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	Vec<Student_info> did, didnt;
+
+	Student_info student;
+	while (read(cin, student))
+	{
+		if (did_all_hw(student))
+			did.push_back(student);
+		else
+			didnt.push_back(student);
+	}
+
+	if (did.empty())
+	{
+		cout << "No student did all the homework!" << endl;
+		return 1;
+	}
+	if (didnt.empty())
+	{
+		cout << "Every student did all the homework!" << endl;
+		return 1;
+	}
+
+	write_analysis(cout, "median", median_analysis, did, didnt);
+	write_analysis(cout, "average", average_analysis, did, didnt);
+	write_analysis(cout, "median of homework turned in",
+				   optimistic_median_analysis, did, didnt);
+
+	return 0;
+}
